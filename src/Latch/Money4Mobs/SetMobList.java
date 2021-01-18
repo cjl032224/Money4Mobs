8d7feeb123098f1a719009201365bca619ca6052
Cody <codylatiolais2011@gmail.com>
2021-01-18 09:13:19 -0600

    Removed some 1.16 mobs and updated plugin.yml to 1.15 api-version

--- src/Latch/Money4Mobs/SetMobList.java
+++ src/Latch/Money4Mobs/SetMobList.java
@@ -34,7 +34,6 @@ public class SetMobList {
         mobList.add(new MobModel("Ghast", 30, 30, true, false, null));
         mobList.add(new MobModel("Giant", 1, 1, true, false, null));
         mobList.add(new MobModel("Guardian", 75, 75, true, false, null));
-        mobList.add(new MobModel("Hoglin", 50, 50, true, false, null));
         mobList.add(new MobModel("Husk", 30, 30, true, false, null));
         mobList.add(new MobModel("Illusioner", 250, 250, true, false, null));
         mobList.add(new MobModel("IronGolem", 10, 10, true, false, null));
@@ -48,7 +47,6 @@ public class SetMobList {
         mobList.add(new MobModel("Phantom", 45, 45, true, false, null));
         mobList.add(new MobModel("Pig", 3, 3, true, false, null));
         mobList.add(new MobModel("PigZombie", 45, 45, true, false, null));
-        mobList.add(new MobModel("PiglinBrute", 75, 75, true, false, null));
         mobList.add(new MobModel("Pillager", 25, 25, true, false, null));
         mobList.add(new MobModel("Player", 25, 25, true, false, null));
         mobList.add(new MobModel("PolarBear", 250, 250, true, false, null));
@@ -66,7 +64,6 @@ public class SetMobList {
         mobList.add(new MobModel("Spider", 15, 15, true, false, null));
         mobList.add(new MobModel("Squid", 10, 10, true, false, null));
         mobList.add(new MobModel("Stray", 25, 25, true, false, null));
-        mobList.add(new MobModel("Strider", 50, 50, true, false, null));
         mobList.add(new MobModel("TraderLlama", 150, 150, true, false, null));
         mobList.add(new MobModel("TropicalFish", 5, 5, true, false, null));
         mobList.add(new MobModel("Turtle", 20, 20, true, false, null));
