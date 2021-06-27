9dee03f5d07836c31ff1a333a260cf5b0ced757f
Cody Latiolais <codylatiolais2011@gmail.com>
2021-06-27 16:54:45 -0500

    Updated plugin version to 1.6.1

--- src/Latch/Money4Mobs/Managers/MobConfigManager.java
+++ src/Latch/Money4Mobs/Managers/MobConfigManager.java
@@ -20,8 +20,6 @@ public class MobConfigManager {
     private static final List<MobModel> mobListFromConfig = new ArrayList<MobModel>();
     public static FileConfiguration mobsCfg;
     public static File mobsFile;
-    public static final String VERSION_NUMBER = "1.6.0";
-    private static final String VERSION = "version";
     private static final String MOBS = "mobs.";
     private static final String DROPS_ITEM = ".drops.item-";
 
