c1328156864766f6543ebd0b0427df25a1cd62af
Cody Latiolais <codylatiolais2011@gmail.com>
2021-09-22 12:53:04 -0500

    updated version and added mobName option for reward and substract messages

--- src/Latch/Money4Mobs/Managers/ConfigFileManager.java
+++ src/Latch/Money4Mobs/Managers/ConfigFileManager.java
@@ -17,7 +17,7 @@ public class ConfigFileManager {
     public static File configFile;
     public static FileConfiguration mobsCfg;
     public static File mobsFile;
-    public static final String VERSION_NUMBER = "1.7.0";
+    public static final String VERSION_NUMBER = "1.7.1";
     private static final String VERSION = "version";
 
     // Set up mobs.yml configuration file
